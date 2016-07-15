88813b705026eae8a3e1945d82808e33b6268dee
caiqiujun <caiqiujun@123feng.com>
2016-07-15 15:23:17 +0800

    Initial Commit

---
+++ UIAlertViewTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIAlertViewTest
+//
+//  Created by IMACYF0003 on 16/7/15.
+//  Copyright © 2016年 IMACYF0003. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
